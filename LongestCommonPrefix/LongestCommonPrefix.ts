c3d322481cd67b113eb658005067093a769f1d15
dunleavyjack <dunleavyjack@gmail.com>
2022-08-28 22:32:13 -0400

    First bad version and longest commone prefix

--- LongestCommonPrefix/LongestCommonPrefix.ts
+++ LongestCommonPrefix/LongestCommonPrefix.ts
@@ -1,15 +1,12 @@
 const longestCommonPrefix = (strs: string[]): string => {
-    // Initialize an initial match
-    let initialMatch = strs[0];
-    let end = initialMatch.length - 1;
-
-    // Iterate through the list to find similar vfalues
-    for (let i: number = 0; i < strs.length; i++) {
-        console.log(strs[i]);
+    // Find smallest string in strings
+    let max = Infinity;
+    for (let str of strs) {
+        max = Math.min(max, str.length);
     }
+    console.log(max);
 
-    console.log('this is the initial match length', end);
-    return initialMatch;
+    return '';
 };
 
 console.log(longestCommonPrefix(['flower', 'flow', 'flight']));

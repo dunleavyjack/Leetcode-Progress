7e0f5a12da0992f91c775a11a9d1a2ff5c594950
dunleavyjack <dunleavyjack@gmail.com>
2022-09-17 21:18:33 -0400

    Added comments for remove duplicates solution

--- RemoveDuplicatesII/RemoveDuplicatesII.ts
+++ RemoveDuplicatesII/RemoveDuplicatesII.ts
@@ -3,27 +3,26 @@ type HashMap = {
 }
 
 const removeDuplicates = (nums: number[]): number[] => {
+  // Create hash map to track duplicate values
   let frequencyCounter: HashMap = {};
 
+  // Count frequency of numbers in nums array
   for (let num of nums) {
     frequencyCounter[num]
       ? (frequencyCounter[num] += 1)
       : (frequencyCounter[num] = 1)
   }
 
+  // While i is in bounds, check number of nums array with frequency
   let i = 0;
   while (i < nums.length) {
     let frequency = frequencyCounter[nums[i]]
-    console.log("START:", i, nums[i], frequency)
-    console.log(nums);
-    if (frequency === 1) i++
-    if (frequency === 2) i += 2
-    if (frequency > 2) {
+    if (frequency === 1) i++ // If 1, skip one place
+    if (frequency === 2) i += 2 // If 2, skip two places
+    if (frequency > 2) { // If > 2, skip two places and remove the rest
       i += 2
       nums.splice(i, frequency - 2)
     }
-    console.log("End:", i, nums[i], frequency)
-    console.log(nums);
   }
 
   return nums

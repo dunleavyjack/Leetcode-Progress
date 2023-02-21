a389acafda7cbba1ff634a934c2d9bfbbd919001
dunleavyjack <dunleavyjack@gmail.com>
2023-02-20 23:06:59 -0600

    cleanup for middle of linked list

--- Solutions/876-MiddleOfLinkedList/MiddleOfLinkedList.ts
+++ Solutions/876-MiddleOfLinkedList/MiddleOfLinkedList.ts
@@ -1,24 +1,14 @@
 import { ListNode } from '../../Definitions/ListNode';
 
-function middleNode(head: ListNode | null): ListNode | null {
-    // Find the length of the linked list
-    // If head exists we can assume length is at least 1
-    let lengthOfList: number = 0;
-    let middleOfList: number;
-    let currentNode: ListNode | null = head;
-    let middleNode: ListNode | null = head;
+function detectCycle(head: ListNode | null): ListNode | null {
+    let nodes = new Set<ListNode>();
+    let currentNode = head;
 
-    while (currentNode !== null) {
-        lengthOfList++;
+    while (currentNode) {
+        if (nodes.has(currentNode)) return currentNode;
+        else nodes.add(currentNode);
         currentNode = currentNode.next;
     }
 
-    // Using the length, find the middle
-    middleOfList = Math.floor(lengthOfList / 2);
-
-    for (let i: number = 0; i < middleOfList; i++) {
-        middleNode = middleNode?.next || null;
-    }
-
-    return middleNode;
+    return null;
 }

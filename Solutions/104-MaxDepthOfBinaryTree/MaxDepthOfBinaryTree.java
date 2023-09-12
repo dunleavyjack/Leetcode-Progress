cccfdc04e76dbf24dc05acf175cf273e187f2448
dunleavyjack <dunleavyjack@gmail.com>
2023-09-11 22:34:04 -0500

    improved java solution for max depth of binary tree

--- Solutions/104-MaxDepthOfBinaryTree/MaxDepthOfBinaryTree.java
+++ Solutions/104-MaxDepthOfBinaryTree/MaxDepthOfBinaryTree.java
@@ -2,8 +2,6 @@ import Definitions.TreeNode.TreeNode;
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        return root != null ? 1 + Math.max(maxDepth(root.left), maxDepth(root.right)) : 0;
     }
 }

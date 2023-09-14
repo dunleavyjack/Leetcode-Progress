80562f17350938057942b78a4b3d8436eb295672
dunleavyjack <dunleavyjack@gmail.com>
2023-09-13 23:45:16 -0500

    bfs java solution for search binary search tree

---
+++ Solutions/700-SearchBST/SearchBinarySearchTree.java
@@ -0,0 +1,16 @@
+import Definitions.TreeNode.*;
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null)
+            return null;
+
+        if (root.val == val)
+            return root;
+
+        TreeNode searchRight = searchBST(root.right, val);
+        TreeNode searchLeft = searchBST(root.left, val);
+
+        return searchRight == null ? searchLeft : searchRight;
+    }
+}

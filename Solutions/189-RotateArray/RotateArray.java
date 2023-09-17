9e31e06f63a282022badbcea34ca4adbfa00fc44
dunleavyjack <dunleavyjack@gmail.com>
2023-09-16 23:27:37 -0500

    java solution for LC189 rotate array

---
+++ Solutions/189-RotateArray/RotateArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] newResult = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            newResult[(i + k) % nums.length] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = newResult[i];
+        }
+
+    }
+}

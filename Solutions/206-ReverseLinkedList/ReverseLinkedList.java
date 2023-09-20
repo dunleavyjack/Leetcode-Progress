ce15a1084ccd5f89c50debc18ec8830f78afad8d
dunleavyjack <dunleavyjack@gmail.com>
2023-09-19 21:39:29 -0500

    java solution for reversed linked list

---
+++ Solutions/206-ReverseLinkedList/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+import Definitions.ListNode.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+
+        return prev;
+    }
+}

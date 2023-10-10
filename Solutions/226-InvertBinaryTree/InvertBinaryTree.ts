b5e4738026b6d39b0a2c52fcbc1a48b727ef5338
dunleavyjack <dunleavyjack@gmail.com>
2023-10-09 22:37:35 -0500

    ts solution for LC226 invert binary tree

---
+++ Solutions/226-InvertBinaryTree/InvertBinaryTree.ts
@@ -0,0 +1,14 @@
+import { TreeNode } from '../../Definitions/TreeNode/TreeNode';
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if (!root) return null;
+
+    const temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+}

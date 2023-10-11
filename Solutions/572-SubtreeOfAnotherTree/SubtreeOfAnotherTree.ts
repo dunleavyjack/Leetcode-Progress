87a8aa7f0bcbcf5337b289d3b4622da5568cbe8d
dunleavyjack <dunleavyjack@gmail.com>
2023-10-10 23:27:17 -0500

    ts solution for LC572 subtree of another tree

---
+++ Solutions/572-SubtreeOfAnotherTree/SubtreeOfAnotherTree.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from '../../Definitions/TreeNode/TreeNode';
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if (!subRoot) return true;
+    if (!root) return false;
+
+    if (sameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+    function sameTree(root: TreeNode | null, sub: TreeNode | null): boolean {
+        if (!root && !sub) return true;
+        if (root && sub && root.val === sub.val) {
+            return (
+                sameTree(root.left, sub.left) && sameTree(root.right, sub.right)
+            );
+        }
+        return false;
+    }
+}

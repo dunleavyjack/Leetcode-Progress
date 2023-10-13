e13d47badb7a85102941abc041bba8c7117889fd
dunleavyjack <dunleavyjack@gmail.com>
2023-10-13 00:02:42 -0500

    ts solution for LC42 Trapping Rain Water (hard)

---
+++ Solutions/42-TrappingRainWater/TrappingRainWater.ts
@@ -0,0 +1,21 @@
+function trap(height: number[]): number {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = height[left];
+    let rightMax = height[right];
+    let water = 0;
+
+    while (left < right) {
+        if (leftMax < rightMax) {
+            left += 1;
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left];
+        } else {
+            right -= 1;
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right];
+        }
+    }
+
+    return water;
+}

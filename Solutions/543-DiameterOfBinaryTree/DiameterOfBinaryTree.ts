71dbe0aa7ef3e840bc2e0ad649e0f1e6ae58e6e4
dunleavyjack <dunleavyjack@gmail.com>
2023-11-19 18:02:59 -0500

    ts solution for LC543

---
+++ Solutions/543-DiameterOfBinaryTree/DiameterOfBinaryTree.ts
@@ -0,0 +1,18 @@
+import { TreeNode } from '../../Definitions';
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let result: number[] = [0];
+
+    const dfs = (root) => {
+        if (!root) return -1;
+        const left = dfs(root.left);
+        const right = dfs(root.right);
+
+        result[0] = Math.max(result[0], 2 + left + right);
+
+        return 1 + Math.max(left, right);
+    };
+
+    dfs(root);
+    return result[0];
+}

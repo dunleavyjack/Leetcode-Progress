7e56f21fb5b473240bad19019421f9bb91d9aa7e
dunleavyjack <dunleavyjack@gmail.com>
2023-12-11 21:40:24 -0600

    ts solution for LC1464

--- Solutions/1464-MaximumProductOfTwoElementsInArray/MaximumProductOfTwoElementsInArray.ts
+++ Solutions/1464-MaximumProductOfTwoElementsInArray/MaximumProductOfTwoElementsInArray.ts
@@ -1,11 +1,21 @@
 function maxProduct(nums: number[]): number {
-    let max: number = 0;
+    let max = 0;
+    let maxIndex = 0;
+
+    let secondMax = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (i != j) max = Math.max(max, (nums[i] - 1) * (nums[j] - 1));
+        if (nums[i] > max) {
+            max = nums[i];
+            maxIndex = i;
         }
     }
 
-    return max;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > secondMax && i !== maxIndex) {
+            secondMax = nums[i];
+        }
+    }
+
+    return (max - 1) * (secondMax - 1);
 }

5a0229dabe4c959c6e2a378470d04cce54b6c192
dunleavyjack <dunleavyjack@gmail.com>
2023-12-15 06:53:50 -0600

    ts solution for LC128

--- Solutions/128-ConsecutiveMaxSequence/ConsecutiveMaxSequence.ts
+++ Solutions/128-ConsecutiveMaxSequence/ConsecutiveMaxSequence.ts
@@ -1,13 +1,15 @@
 function longestConsecutive(nums: number[]): number {
     let set = new Set<number>(nums);
     let maxSeq = 0;
+    let potentialSeq = 0;
 
     for (let num of nums) {
         if (!set.has(num - 1)) {
-            let potentialMaxSeq = 1;
-            while (set.has(num + potentialMaxSeq)) potentialMaxSeq++;
-
-            maxSeq = Math.max(potentialMaxSeq, maxSeq);
+            while (set.has(num + potentialSeq)) {
+                potentialSeq++;
+            }
+            maxSeq = Math.max(maxSeq, potentialSeq);
+            potentialSeq = 0;
         }
     }
 

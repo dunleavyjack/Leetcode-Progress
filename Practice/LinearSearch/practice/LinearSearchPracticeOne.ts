656429dd5dfba43019623d05666f2d79f7180c06
dunleavyjack <dunleavyjack@gmail.com>
2023-12-24 13:28:40 -0500

    removed old solutions

--- Practice/LinearSearch/practice/LinearSearchPracticeOne.ts
+++
@@ -1,9 +0,0 @@
-const linearSearch = (nums: number[], target: number): number => {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) return i
-  }
-
-  return -1;
-}
-
-console.log(linearSearch([1, 2, 3, 4, 5], 5))

6328a5d5845bc584258a0008606b81e5a07902fa
dunleavyjack <dunleavyjack@gmail.com>
2024-01-16 23:34:37 -0600

    updated ts solution for LC1207

--- Solutions/1207-UniqueOccurences/UniqueOccurences.ts
+++ Solutions/1207-UniqueOccurences/UniqueOccurences.ts
@@ -1,18 +1,15 @@
 function uniqueOccurrences(arr: number[]): boolean {
     // Init an empty hash map.
-    let freqCounter = new Map<number, number>();
-    let uniqueOccurrences = new Set<number>();
+    let freqCounter = new Map<number, number>(); // { num: freq }
+    let uniqueFrequencies = new Set<number>();
 
     // Do a frequency count for each element in the array.
-    for (let a of arr) {
-        if (freqCounter.has(a)) freqCounter.set(a, freqCounter.get(a)! + 1);
-        else freqCounter.set(a, 1);
-    }
+    for (let num of arr) freqCounter.set(num, (freqCounter.get(num) || 0) + 1);
 
     // Add values from the hashmap
-    for (let [_key, value] of freqCounter) {
-        if (uniqueOccurrences.has(value)) return false;
-        else uniqueOccurrences.add(value);
+    for (let [_num, freq] of freqCounter) {
+        if (uniqueFrequencies.has(freq)) return false;
+        else uniqueFrequencies.add(freq);
     }
 
     return true;

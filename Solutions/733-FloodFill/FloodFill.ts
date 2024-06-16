478160f387de27ccf68e32e43593e20519c53e16
dunleavyjack <dunleavyjack@gmail.com>
2024-06-16 14:36:52 -0500

    improved ts solution for LC733

--- Solutions/733-FloodFill/FloodFill.ts
+++ Solutions/733-FloodFill/FloodFill.ts
@@ -1,33 +1,35 @@
 function floodFill(
-    image: number[][],
-    sr: number,
-    sc: number,
-    color: number
+	image: number[][],
+	sr: number,
+	sc: number,
+	color: number
 ): number[][] {
-    if (image[sr][sc] === color) return image;
-    fill(image, sr, sc, image[sr][sc], color);
-    return image;
+	if (image[sr][sc] !== color) {
+		fill(image, sr, sc, image[sr][sc], color);
+	}
+
+	return image;
 }
 
-const fill = (
-    image: number[][],
-    sr: number,
-    sc: number,
-    startingColor: number,
-    color: number
-): void => {
-    if (
-        sr < 0 ||
-        sc < 0 ||
-        sr >= image.length ||
-        sc >= image[0].length ||
-        image[sr][sc] !== startingColor
-    )
-        return;
+function fill(
+	image: number[][],
+	sr: number,
+	sc: number,
+	startingColor: number,
+	color: number
+) {
+	if (
+		sr < 0 ||
+		sc < 0 ||
+		sr >= image.length ||
+		sc >= image[0].length ||
+		image[sr][sc] !== startingColor
+	)
+		return;
 
-    image[sr][sc] = color;
-    fill(image, sr - 1, sc, startingColor, color);
-    fill(image, sr + 1, sc, startingColor, color);
-    fill(image, sr, sc - 1, startingColor, color);
-    fill(image, sr, sc + 1, startingColor, color);
-};
+	image[sr][sc] = color;
+	fill(image, sr - 1, sc, startingColor, color);
+	fill(image, sr + 1, sc, startingColor, color);
+	fill(image, sr, sc - 1, startingColor, color);
+	fill(image, sr, sc + 1, startingColor, color);
+}

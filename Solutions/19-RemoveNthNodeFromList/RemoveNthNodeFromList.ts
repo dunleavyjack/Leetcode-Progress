da48888129049794956d6a3d19087697cf1c1c98
dunleavyjack <dunleavyjack@gmail.com>
2024-09-11 08:49:22 -0500

    TS solution for LC19 remove nth node from list

---
+++ Solutions/19-RemoveNthNodeFromList/RemoveNthNodeFromList.ts
@@ -0,0 +1,25 @@
+import { ListNode } from "../../Types";
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  const dummy = new ListNode();
+  dummy.next = head;
+  let curr = dummy;
+  let slow = dummy;
+
+  // Increment curr n steps ahead of slow
+  while (n && curr) {
+    curr = curr.next;
+    n--;
+  }
+
+  // Increment both until curr reaches the end
+  while (curr.next) {
+    curr = curr.next;
+    slow = slow.next;
+  }
+
+  // Slow will be directly before the node to skip. Skip it!
+  slow.next = slow?.next?.next ?? null;
+
+  return dummy.next;
+}

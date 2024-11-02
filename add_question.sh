d9f8818c3ecc37b719bfbde0d68e0535f8f0a64c
dunleavyjack <dunleavyjack@gmail.com>
2024-11-02 14:20:40 -0500

    chore(scripts): updated file generator to include markdown notes section

--- add_question.sh
+++ add_question.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# A script for generating a folder and empty TypeScript file based on a provided Leetcode question name.
+# A script for generating a folder, an empty TypeScript file, and a markdown notes file based on a provided Leetcode question name.
 #
-# Example: 
+# Example:
 #
-# `npm run quesion 999 TestQuestion` 
+# `npm run question 999 TestQuestion`
 #
-# Generates a folder named 999-TestQuestion containing a file called TestQuestion.ts.
+# Generates a folder named 999-TestQuestion containing files:
+# - TestQuestion.ts
+# - TestQuestion.notes.md with initial content
 
 # Check if two arguments were provided
 if [ $# -ne 2 ]; then
@@ -21,17 +23,29 @@ folder_name="${question_number}-${question_name}"
 # Create a directory with the specified format
 full_folder_path="./Solutions/$folder_name"
 if [ ! -d "$full_folder_path" ]; then
-  mkdir "$full_folder_path"
+  mkdir -p "$full_folder_path"
   echo "Created folder: $full_folder_path"
 else
   echo "Folder '$full_folder_path' already exists. Skipping folder creation."
 fi
 
 # Create a TypeScript file with the specified name inside the folder
-file_name="$full_folder_path/$question_name.ts"
-if [ ! -e "$file_name" ]; then
-  touch "$file_name"
-  echo "Created TypeScript file: $file_name"
+ts_file_name="$full_folder_path/$question_name.ts"
+if [ ! -e "$ts_file_name" ]; then
+  touch "$ts_file_name"
+  echo "Created TypeScript file: $ts_file_name"
 else
-  echo "File '$file_name' already exists. Skipping file creation."
+  echo "File '$ts_file_name' already exists. Skipping file creation."
+fi
+
+# Format the question name with spaces between capital letters
+formatted_question_name=$(echo "$question_name" | sed 's/\([a-z]\)\([A-Z]\)/\1 \2/g')
+
+# Create a Markdown notes file with the specified format and initial content
+md_file_name="$full_folder_path/$question_name.notes.md"
+if [ ! -e "$md_file_name" ]; then
+  echo "# Notes: LC $question_number $formatted_question_name" >"$md_file_name"
+  echo "Created Markdown notes file: $md_file_name"
+else
+  echo "File '$md_file_name' already exists. Skipping file creation."
 fi

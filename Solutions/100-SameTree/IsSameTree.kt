72967e93c6a626547cbd02157a54c4b7d4cc25c4
dunleavyjack <dunleavyjack@gmail.com>
2024-11-09 17:32:54 -0600

    kt solution for LC100 Same Tree

---
+++ Solutions/100-SameTree/IsSameTree.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if(p == null && q == null) return true;
+
+        if(
+            p == null ||
+            q == null ||
+            p.`val` != q.`val`
+        ) return false
+
+        val isSameLeft = isSameTree(p.left, q.left)
+        val isSameRight = isSameTree(p.right, q.right)
+    
+        return isSameLeft && isSameRight
+    }
+}
--- Solutions/100-SameTree/SameTree.ts
+++ Solutions/100-SameTree/SameTree.ts
@@ -1,9 +1,9 @@
-import { TreeNode } from "../../types";
+import { TreeNode } from "../../Types";
 
-function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-	if (!p && !q) return true;
+export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (!p && !q) return true;
 
-	if (!p || !q || p.val !== q.val) return false;
+  if (!p || !q || p.val !== q.val) return false;
 
-	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }

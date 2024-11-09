48dc8f666bf3ca2c264e109c0abae2d010e039ce
dunleavyjack <dunleavyjack@gmail.com>
2024-11-09 14:09:07 -0600

    kt solution for LC226 invert binary tree

---
+++ Solutions/226-InvertBinaryTree/InvertBinaryTree.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if(root == null) return null
+
+        val temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        invertTree(root.left)
+        invertTree(root.right)
+
+        return root
+    }
+}

684029d0e0f45eb0bebf4a3839c55d3e350736ae
dunleavyjack <dunleavyjack@gmail.com>
2024-11-08 21:38:26 -0600

    ts solution for LC110 balanced binary tree

--- Solutions/110-BalancedBinaryTree/BalancedBinaryTree.ts
+++ Solutions/110-BalancedBinaryTree/BalancedBinaryTree.ts
@@ -1,15 +1,19 @@
 import { TreeNode } from "../../Types";
 
-function isBalanced(root: TreeNode | null): boolean {
-	function dfs(root) {
-		if (!root) return [true, 0];
+export function isBalanced(root: TreeNode | null): boolean {
+  function dfs(node: TreeNode | null): [boolean, number] {
+    if (!node) return [true, 0];
 
-		const left = dfs(root.left);
-		const right = dfs(root.right);
-		const balanced = left[0] && right[0] && Math.abs(left[1] - right[1]) <= 1;
+    const [isLeftBalanced, maxLeftHeight] = dfs(node.left);
+    const [isRightBalanced, maxRightHeight] = dfs(node.right);
+    const isBalanced =
+      isLeftBalanced &&
+      isRightBalanced &&
+      Math.abs(maxLeftHeight - maxRightHeight) <= 1;
 
-		return [balanced, 1 + Math.max(left[1] + right[1])];
-	}
+    return [isBalanced, Math.max(maxLeftHeight, maxRightHeight) + 1];
+  }
 
-	return dfs(root);
+  const [isBalanced] = dfs(root);
+  return isBalanced;
 }

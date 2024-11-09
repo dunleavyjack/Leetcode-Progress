0dec7dce5e7b367b6252a05fd78cd0b6e4fc3f71
dunleavyjack <dunleavyjack@gmail.com>
2024-11-09 17:06:51 -0600

    kt solution for LC110 Balanced Binary Tree

---
+++ Solutions/110-BalancedBinaryTree/BalancedBinaryTree.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun isBalanced(root: TreeNode?): Boolean {
+       val (isTreeBalanced) = dfs(root)
+       return isTreeBalanced 
+    }
+
+    fun dfs(node: TreeNode?): Pair<Boolean, Int> {
+        if(node == null) return Pair(true, 0)
+
+        val (isBalancedLeft, maxHeightLeft) = dfs(node.left)
+        val (isBalancedRight, maxHeightRight) = dfs(node.right)
+        val isTreeBalanced = 
+            isBalancedLeft &&
+            isBalancedRight &&
+            Math.abs(maxHeightLeft - maxHeightRight) <= 1
+        val maxHeight = 1 + Math.max(maxHeightLeft, maxHeightRight)
+
+        return Pair(isTreeBalanced, maxHeight)
+    }
+}
--- Solutions/110-BalancedBinaryTree/BalancedBinaryTree.ts
+++ Solutions/110-BalancedBinaryTree/BalancedBinaryTree.ts
@@ -4,16 +4,18 @@ export function isBalanced(root: TreeNode | null): boolean {
   function dfs(node: TreeNode | null): [boolean, number] {
     if (!node) return [true, 0];
 
-    const [isLeftBalanced, maxLeftHeight] = dfs(node.left);
-    const [isRightBalanced, maxRightHeight] = dfs(node.right);
-    const isBalanced =
-      isLeftBalanced &&
-      isRightBalanced &&
-      Math.abs(maxLeftHeight - maxRightHeight) <= 1;
+    const [isBalancedLeft, maxHeightLeft] = dfs(node.left);
+    const [isBalancedRight, maxHeightRight] = dfs(node.right);
 
-    return [isBalanced, Math.max(maxLeftHeight, maxRightHeight) + 1];
+    const isBalanced =
+      isBalancedLeft &&
+      isBalancedRight &&
+      Math.abs(maxHeightLeft - maxHeightRight) <= 1;
+    const maxHeight = 1 + Math.max(maxHeightLeft, maxHeightRight);
+
+    return [isBalanced, maxHeight];
   }
 
-  const [isBalanced] = dfs(root);
-  return isBalanced;
+  const [isTreeBalanced] = dfs(root);
+  return isTreeBalanced;
 }

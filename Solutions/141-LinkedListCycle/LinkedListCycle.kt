d25d6850098b64402aa64b2bcd112c8e6690e9f0
dunleavyjack <dunleavyjack@gmail.com>
2024-11-13 21:58:42 -0600

    kt solution for LC141 Linked List Cycle

---
+++ Solutions/141-LinkedListCycle/LinkedListCycle.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        var slow = head;
+        var fast = head;
+
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next
+            fast = fast.next.next
+
+            if(slow == fast) return true
+        }
+
+        return false
+    }
+}

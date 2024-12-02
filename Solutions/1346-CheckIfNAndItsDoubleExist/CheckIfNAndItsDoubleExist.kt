ffe0f3f2c76d171856604ed5e385e02de09819a8
dunleavyjack <dunleavyjack@gmail.com>
2024-12-01 19:06:08 -0500

    kt solution for LC1346 check if n and its double exist

---
+++ Solutions/1346-CheckIfNAndItsDoubleExist/CheckIfNAndItsDoubleExist.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun checkIfExist(arr: IntArray): Boolean {
+        val set: MutableSet<Int> = mutableSetOf();
+
+        for(num in arr){
+            if(set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) return true;
+            else set.add(num)
+        } 
+
+        return false;
+    }
+}

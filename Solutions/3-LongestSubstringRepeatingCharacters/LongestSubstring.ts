04a8586b489f8fd8618c906fd7242f3f741dba20
dunleavyjack <dunleavyjack@gmail.com>
2024-12-13 20:33:33 -0600

    improved ts solution for LC3

--- Solutions/3-LongestSubstringRepeatingCharacters/LongestSubstring.ts
+++ Solutions/3-LongestSubstringRepeatingCharacters/LongestSubstring.ts
@@ -1,19 +1,28 @@
 function lengthOfLongestSubstring(s: string): number {
+  // Create a set to keep track of unique chars, and a max value to store the result.
   const uniqueChars = new Set<string>();
-  let left = 0;
   let max = 0;
 
-  for (let right = 0; right < s.length; right++) {
-    // If a duplicate exists in the set, keep removing the lefternmost values until the new right value can be added
+  // Two pointers, left and right.
+  let left = 0;
+  let right = 0;
+
+  // Iterate until right is out of bounds.
+  while (right < s.length) {
+    // First, check to see if this char is already in the set.
+    // If it does, keep deleting and incrementing the left value.
     while (uniqueChars.has(s[right])) {
       uniqueChars.delete(s[left]);
       left++;
     }
 
-    // Always add the right value
+    // "Happy path". Add the right value to the set and compare the total length with the max.
     uniqueChars.add(s[right]);
-    max = Math.max(max, uniqueChars.size);
+    max = Math.max(right - left + 1, max);
+
+    right++;
   }
 
+  // Return the result.
   return max;
 }

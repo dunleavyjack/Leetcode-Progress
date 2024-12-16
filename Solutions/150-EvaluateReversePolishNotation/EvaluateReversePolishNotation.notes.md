80493bd8283cd392833c94dc9c2c1bef3c0c6235
dunleavyjack <dunleavyjack@gmail.com>
2024-12-16 07:44:27 -0600

    updated solution and notes for LC150 reverse polish notation

---
+++ Solutions/150-EvaluateReversePolishNotation/EvaluateReversePolishNotation.notes.md
@@ -0,0 +1,9 @@
+# Notes
+
+For this one, you can use a stack to keep track of numbers as you
+iterate through the input. When an operator is reached, retrieve
+the last two values from the stack using two separate `.pop()` calls.
+Then, preform the operation on those two numbers and, this is key,
+push the result back into the stack. There doesn't need to be
+a separate result variable. You need the running total to be inside
+the stack so it can be used for future computation.
--- Solutions/150-EvaluateReversePolishNotation/EvaluateReversePolishNotation.ts
+++ Solutions/150-EvaluateReversePolishNotation/EvaluateReversePolishNotation.ts
@@ -2,26 +2,36 @@ function evalRPN(tokens: string[]): number {
   let stack: number[] = [];
 
   for (let token of tokens) {
+    // Addition
     if (token === "+") {
       const lastNum = stack.pop() as number;
-      const secondToLastNum = stack.pop() as number;
-      stack.push(secondToLastNum + lastNum);
-    } else if (token === "*") {
-      const lastNum = stack.pop() as number;
-      const secondToLastNum = stack.pop() as number;
-      stack.push(secondToLastNum * lastNum);
-    } else if (token === "/") {
-      const lastNum = stack.pop() as number;
-      const secondToLastNum = stack.pop() as number;
-      stack.push(Math.trunc(secondToLastNum / lastNum));
+      const firstNum = stack.pop() as number;
+      stack.push(firstNum + lastNum);
+
+      // Subtraction
     } else if (token === "-") {
       const lastNum = stack.pop() as number;
-      const secondToLastNum = stack.pop() as number;
-      stack.push(secondToLastNum - lastNum);
+      const firstNum = stack.pop() as number;
+      stack.push(firstNum - lastNum);
+
+      // Division (truncating towards 0)
+    } else if (token === "/") {
+      const lastNum = stack.pop() as number;
+      const firstNum = stack.pop() as number;
+      stack.push(Math.trunc(firstNum / lastNum));
+
+      // Multiplication
+    } else if (token === "*") {
+      const lastNum = stack.pop() as number;
+      const firstNum = stack.pop() as number;
+      stack.push(firstNum * lastNum);
+
+      // Otherwise, push the number to the stack
     } else {
       stack.push(Number(token));
     }
   }
 
+  // Since the stack contains the running total, return it
   return stack[0];
 }

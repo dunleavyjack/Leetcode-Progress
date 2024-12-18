55fa7bc386b50909d62695a8414cc5778dcc8334
dunleavyjack <dunleavyjack@gmail.com>
2024-12-17 22:47:29 -0600

    updated solution for LC739 Daily Temperatures

--- Solutions/739-DailyTemperatures/DailyTemperatures.ts
+++ Solutions/739-DailyTemperatures/DailyTemperatures.ts
@@ -1,16 +1,25 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-  let result = Array(temperatures.length).fill(0);
-  let stack: number[] = []; // Contains the index of values in a monotonic, decreasing stack
+  // Will contain the amount of days each index in temperatures has to wait until a warmer day occurs
+  let answer = new Array(temperatures.length).fill(0);
+  // Will contain the indexes of days that have not yet reached a warmer day
+  let stack: number[] = [];
 
+  // Iterate through each temperature in temperatures
   for (let i = 0; i < temperatures.length; i++) {
-    // While current num greater than last value in stack, remove it and assign the distance to the result
-    while (stack && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+    // If the last value in the stack is less than the current temperature, we've found a warmer day!
+    // Pop that value, set the difference to the answer array, and continue to check through the stack
+    while (
+      stack.length &&
+      temperatures[stack[stack.length - 1]] < temperatures[i]
+    ) {
       const lastIndexInStack = stack.pop() as number;
-      const daysToWait = i - lastIndexInStack;
-      result[lastIndexInStack] = daysToWait;
+      answer[lastIndexInStack] = i - lastIndexInStack;
     }
+
+    // Always, push the current value to the stack. We haven't found a warmer day for it yet.
     stack.push(i);
   }
 
-  return result;
+  // Return the answer array
+  return answer;
 }

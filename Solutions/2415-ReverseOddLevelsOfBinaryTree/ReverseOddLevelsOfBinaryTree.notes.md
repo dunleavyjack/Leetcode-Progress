0ec07a9e522a44859973f5faf31752850b0615a1
dunleavyjack <dunleavyjack@gmail.com>
2024-12-21 18:36:33 -0600

    ts solution for LC2415 reverse odd levels in binary tree

---
+++ Solutions/2415-ReverseOddLevelsOfBinaryTree/ReverseOddLevelsOfBinaryTree.notes.md
@@ -0,0 +1,1 @@
+# Notes: LC 2415 Reverse Odd Levels Of Binary Tree
---
+++ Solutions/2415-ReverseOddLevelsOfBinaryTree/ReverseOddLevelsOfBinaryTree.ts
@@ -0,0 +1,34 @@
+import { TreeNode } from "../../Types";
+
+export function reverseOddLevels(root: TreeNode | null): TreeNode | null {
+  const levels: number[][] = [];
+
+  function dfs(node: TreeNode | null, level: number) {
+    if (!node) return null;
+
+    if (!levels[level]) levels.push([]);
+    console.log(`pushing node: ${node.val}`);
+    levels[level].push(node.val);
+
+    dfs(node.left, level + 1);
+    dfs(node.right, level + 1);
+  }
+
+  function reverseLevels(node: TreeNode | null, level: number) {
+    if (!node) return null;
+
+    if (level % 2 !== 0) {
+      const val = levels[level].pop() as number;
+      node.val = val;
+    }
+
+    reverseLevels(node.left, level + 1);
+    reverseLevels(node.right, level + 1);
+
+    return node;
+  }
+
+  dfs(root, 0);
+
+  return reverseLevels(root, 0);
+}

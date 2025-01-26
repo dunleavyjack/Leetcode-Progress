de51f2893e45ec3cd007ac7203bf5ddbdace9162
dunleavyjack <dunleavyjack@gmail.com>
2025-01-25 21:33:59 -0600

    improved ts solution for LC314 binary tree vertical level traversal

--- Solutions/314-BinaryTreeVerticalLevelTraversal/BinaryTreeVerticalLevelTraversal.ts
+++ Solutions/314-BinaryTreeVerticalLevelTraversal/BinaryTreeVerticalLevelTraversal.ts
@@ -1,35 +1,31 @@
 import { TreeNode } from "../../Types";
 
 export function verticalOrder(root: TreeNode | null): number[][] {
-  const verticalLevels = new Map<number, number[]>(); // {level, valuesAtLevel }
-  let minLevel = 0;
+  if (!root) return [];
 
-  function bfs(root: TreeNode | null) {
-    if (!root) return;
+  const verticalLevelsMap = new Map<number, number[]>(); // column number, nodes in that column
+  let minVerticalLevel = 0;
 
-    const queue: [TreeNode, number][] = [[root, 0]]; // [node, level]
+  const queue: [TreeNode, number][] = [[root, 0]];
 
-    while (queue.length) {
-      const [node, level] = queue.shift() as [TreeNode, number];
+  while (queue.length) {
+    const [node, level] = queue.shift() as [TreeNode, number];
 
-      minLevel = Math.min(minLevel, level);
-      verticalLevels.set(level, [
-        ...(verticalLevels.get(level) || []),
-        node.val,
-      ]);
+    verticalLevelsMap.set(level, [
+      ...(verticalLevelsMap.get(level) || []),
+      node.val,
+    ]);
+    minVerticalLevel = Math.min(minVerticalLevel, level);
 
-      if (node.left) queue.push([node.left, level - 1]);
-      if (node.right) queue.push([node.right, level + 1]);
-    }
+    if (node.left) queue.push([node.left, level - 1]);
+    if (node.right) queue.push([node.right, level + 1]);
   }
 
-  bfs(root);
+  console.log(verticalLevelsMap);
 
   let result: number[][] = [];
-
-  for (const [level, valuesAtLevel] of verticalLevels) {
-    result[level + Math.abs(minLevel)] = valuesAtLevel;
+  for (const [level, nodes] of verticalLevelsMap) {
+    result[level - minVerticalLevel] = nodes;
   }
-
   return result;
 }

269e0b2594bc0dbee0241824b51155b99b624c18
dunleavyjack <dunleavyjack@gmail.com>
2025-02-18 22:43:45 -0600

    c# solution for binary search

---
+++ Solutions/704-BinarySearch/BinarySearch.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (int)Math.Floor((left + right) / 2.0);
+            if (nums[mid] == target)
+                return mid;
+
+            if (target < nums[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}

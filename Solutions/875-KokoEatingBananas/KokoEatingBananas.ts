144d87980bc4084e05d5cf9aedb25368c81da603
dunleavyjack <dunleavyjack@gmail.com>
2025-03-12 08:23:08 -0500

    improved ts solution for LC875 koko eating bananas

--- Solutions/875-KokoEatingBananas/KokoEatingBananas.ts
+++ Solutions/875-KokoEatingBananas/KokoEatingBananas.ts
@@ -1,29 +1,21 @@
 function minEatingSpeed(piles: number[], h: number): number {
-  let maxSpeed = Math.max(...piles);
   let minSpeed = 1;
-
-  let minPossibleSpeed = maxSpeed;
+  let maxSpeed = Math.max(...piles);
+  let minValidSpeed = maxSpeed;
 
   while (minSpeed <= maxSpeed) {
     const speed = Math.floor((minSpeed + maxSpeed) / 2);
-    const duration = getEatingDuration(piles, speed);
-    if (duration <= h) {
-      minPossibleSpeed = Math.min(minPossibleSpeed, speed);
+    const timeToEatBananas = piles.reduce(
+      (acc, pile) => acc + Math.ceil(pile / speed),
+      0,
+    );
+    if (timeToEatBananas <= h) {
+      minValidSpeed = Math.min(minValidSpeed, speed);
       maxSpeed = speed - 1;
     } else {
       minSpeed = speed + 1;
     }
   }
 
-  return minPossibleSpeed;
-}
-
-function getEatingDuration(nums: number[], speed: number) {
-  let duration = 0;
-
-  for (let num of nums) {
-    duration += Math.ceil(num / speed);
-  }
-
-  return duration;
+  return minValidSpeed;
 }

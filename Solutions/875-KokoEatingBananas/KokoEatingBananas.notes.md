1ad6f004d50e82baf3787c28b05d648ebeadaa4f
dunleavyjack <dunleavyjack@gmail.com>
2025-03-13 21:22:11 -0500

    improved ts solution for LC875 koko eating bananas

--- Solutions/875-KokoEatingBananas/KokoEatingBananas.notes.md
+++ Solutions/875-KokoEatingBananas/KokoEatingBananas.notes.md
@@ -1,1 +1,16 @@
 # Notes: LC 875 Koko Eating Bananas
+
+This one always confuses me at first, because I don't think to use binary
+search. But, that's the idea!
+
+The trick is to not think about finding a "perfect mathematical" solution.
+Instead, you want to consider all possible solutions between `1` (eating one
+banana per hour), and `h` (the total amount of time before the zoo keeper
+returns).
+
+Once you consider the problem this way, binary search becomes clear. Perform
+binary search like usual, except instead of just using the `mid` element to
+check for a target, use `mid` to calculate the speed. This is done by iterating
+through each `pile` in `piles`, and using `Math.ceil` (in TypeScript), because
+Koko will not continue to the next pile if she eats all the bananas in a single
+pile.
--- Solutions/875-KokoEatingBananas/KokoEatingBananas.ts
+++ Solutions/875-KokoEatingBananas/KokoEatingBananas.ts
@@ -1,21 +1,28 @@
 function minEatingSpeed(piles: number[], h: number): number {
-  let minSpeed = 1;
+  let minSpeed = 0;
   let maxSpeed = Math.max(...piles);
-  let minValidSpeed = maxSpeed;
+
+  // The result: the speed for eating bananans faster than `h`
+  let bestMinSpeed = maxSpeed;
 
   while (minSpeed <= maxSpeed) {
+    // Calcualte the "medium" speed between the min and max pointers
     const speed = Math.floor((minSpeed + maxSpeed) / 2);
-    const timeToEatBananas = piles.reduce(
-      (acc, pile) => acc + Math.ceil(pile / speed),
+    // Use that speed to calculate the amount of time required to eat all bananans
+    const hoursToEatBananas = piles.reduce(
+      (total, piles) => total + Math.ceil(piles / speed),
       0,
     );
-    if (timeToEatBananas <= h) {
-      minValidSpeed = Math.min(minValidSpeed, speed);
+
+    if (hoursToEatBananas <= h) {
+      // Check if the value is less than the current best min and try to find a smaller max speed
+      bestMinSpeed = Math.min(bestMinSpeed, speed);
       maxSpeed = speed - 1;
     } else {
+      // Speed was too small, look for a faster speed
       minSpeed = speed + 1;
     }
   }
 
-  return minValidSpeed;
+  return bestMinSpeed;
 }

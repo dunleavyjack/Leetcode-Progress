d2bee3193110cbb8eb809493a2f15dff097f6f8c
dunleavyjack <dunleavyjack@gmail.com>
2025-03-18 22:05:15 -0500

    updated ts solution for LC981 Time Based Key Value Store

--- Solutions/981-TimeBasedKeyValueStore/TimeBasedKeyValueStore.ts
+++ Solutions/981-TimeBasedKeyValueStore/TimeBasedKeyValueStore.ts
@@ -1,50 +1,53 @@
-type Store = Map<string, Array<[string, number]>>;
-
 class TimeMap {
-  store: Store;
+  map: Map<string, [number, string][]>; // { Key: [time, value] }
 
   constructor() {
-    this.store = new Map();
+    this.map = new Map<string, [number, string][]>();
   }
 
   set(key: string, value: string, timestamp: number): void {
-    const values = this.store.get(key);
-
-    // Key does not previously exist in store
-    if (!values) {
-      this.store.set(key, [[value, timestamp]]);
-      return;
-    }
-
-    const insertionIndex = this.findInsertionIndex(values, timestamp);
-    values.splice(insertionIndex, 0, [value, timestamp]);
-    this.store.set(key, values);
+    const values = this.map.get(key) || [];
+    values.push([timestamp, value]);
+    this.map.set(key, values);
   }
 
   get(key: string, timestamp: number): string {
-    const values = this.store.get(key);
-    if (!values) return "";
+    const values = this.map.get(key) || [];
+    const targetIndex = this.findTargetIndex(values, timestamp);
 
-    const insertionIndex = this.findInsertionIndex(values, timestamp);
-    return insertionIndex > 0 ? values[insertionIndex - 1][0] : "";
+    if (targetIndex >= 0) {
+      return values[targetIndex][1];
+    } else {
+      return "";
+    }
   }
 
-  /**
-   * Finds the index in a sorted array where a provided value should be inserted.
-   */
-  private findInsertionIndex(values: Array<[string, number]>, value: number) {
+  private findTargetIndex(
+    values: [number, string][],
+    timestamp: number,
+  ): number {
     let left = 0;
     let right = values.length - 1;
 
     while (left <= right) {
       const mid = Math.floor((left + right) / 2);
-      if (value < values[mid][1]) {
+      const [currTimestamp] = values[mid];
+
+      if (timestamp < currTimestamp) {
         right = mid - 1;
       } else {
         left = mid + 1;
       }
     }
 
-    return left;
+    // Return one minus the left pointer (the index before!)
+    return left - 1;
   }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */

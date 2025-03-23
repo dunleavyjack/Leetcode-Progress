dc09b143a6f62eb04a424f457d034c88274bee95
dunleavyjack <dunleavyjack@gmail.com>
2025-03-23 11:04:20 -0500

    updated ts solution and notes for LC994 Rotting Oranges

--- Solutions/994-RottingOranges/RottingOranges.notes.md
+++ Solutions/994-RottingOranges/RottingOranges.notes.md
@@ -1,5 +1,20 @@
 # Notes: LC 994 Rotting Oranges
 
-| Time   | Space |
-| ------ | ----- |
-| O(n)   | O(n)  |
+This one requires "Multi-start BFS". Like a lot of similar graph problems, the
+idea is to iterate through all of the cells in the grid. When a rotten orange is
+reached, add it's coords to a "rotten orange coords queue". Also, (important!),
+keep a counter of fresh oranges as well. Since it's possible that not all fresh
+oranges can be made rotten, you need to keep track of this value.
+
+After populating a queue of all the rotten orange coords, you get to the
+Multi-start BFS portion. It's really similar to normal BFS, with one exception.
+Since all rotten oranges are "rotting" at the same time, we want to have a for
+loop within the normal BFS while loop to keep track of how many minutes have
+elapsed. At each iteration of the `while(queue)` in the BFS, store the current
+length of queue in a variable and start a for loop iteration from 0 to the
+length of the "current queue". At the end of the for loop, increment `minutes`
+once. This ensures that we know when each "level" of the BFS occurred.
+
+| Time      | Space     |
+| --------- | --------- |
+| O(n \* m) | O(n \* m) |
--- Solutions/994-RottingOranges/RottingOranges.ts
+++ Solutions/994-RottingOranges/RottingOranges.ts
@@ -1,32 +1,30 @@
 function orangesRotting(grid: number[][]): number {
-  let rottenOranges: [number, number][] = []; // [row, col] of rotten orange
-
-  let minutes = 0;
   let freshOranges = 0;
+  let rottenOrangeCoords: number[][] = [];
+  let minutes = 0;
 
   const directions = [
     [-1, 0],
     [1, 0],
-    [0, 1],
     [0, -1],
+    [0, 1],
   ] as const;
 
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[0].length; c++) {
-      if (grid[r][c] === 2) rottenOranges.push([r, c]);
+      if (grid[r][c] === 2) rottenOrangeCoords.push([r, c]);
       else if (grid[r][c] === 1) freshOranges++;
     }
   }
 
-  while (rottenOranges.length && freshOranges) {
-    const currentLevelSize = rottenOranges.length;
+  while (rottenOrangeCoords.length && freshOranges > 0) {
+    const currentLevelSize = rottenOrangeCoords.length;
     for (let i = 0; i < currentLevelSize; i++) {
-      const [row, col] = rottenOranges.shift()!;
+      const [row, col] = rottenOrangeCoords.shift()!;
 
       for (const [rowDirection, colDirection] of directions) {
         const newRow = row + rowDirection;
         const newCol = col + colDirection;
-
         if (
           newRow < 0 ||
           newCol < 0 ||
@@ -37,9 +35,8 @@ function orangesRotting(grid: number[][]): number {
           continue;
 
         grid[newRow][newCol] = 2; // Make rotten
-        freshOranges--;
-
-        rottenOranges.push([newRow, newCol]);
+        freshOranges--; // Decrement fresh orange count
+        rottenOrangeCoords.push([newRow, newCol]); // Add to rotten oranges queue
       }
     }
 

c34638be5aef96a69ad734c23fefcc1e2c51c735
dunleavyjack <dunleavyjack@gmail.com>
2025-04-02 20:30:47 -0500

    ts solution for LC70 climbing stairs

--- Solutions/70-ClimbingStairs/ClimbingStairs.ts
+++ Solutions/70-ClimbingStairs/ClimbingStairs.ts
@@ -1,11 +1,7 @@
-function climbStairs(n: number): number {
-  const dp = Array(n).fill(0);
-  dp[0] = 1;
-  dp[1] = 2;
+function climbStairs(n: number, memo = {}): number {
+  if (n in memo) return memo[n];
+  if (n === 1 || n === 2) return n;
 
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-
-  return dp[n - 1];
+  memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+  return memo[n];
 }

beac95ca38b7ce65f1c92227814b98781a212fc6
dunleavyjack <dunleavyjack@gmail.com>
2025-04-28 22:09:30 -0500

    updated ts solution for LC146 LRU cache

--- Solutions/146-LeastRecentlyUsedCache/LeastRecentlyUsedCache.easy.ts
+++ Solutions/146-LeastRecentlyUsedCache/LeastRecentlyUsedCache.easy.ts
@@ -1,51 +1,43 @@
-/**
- * This version of LRU Cache doesn't require a Doubly Linked List!
- * And still has O(1) time complexity for retrival times!
- *
- * The idea is to exploit the face that JS/TS HashMaps maintain insertion order.
- *
- * You can do this with a hashmap to map each key to it's value. The real "magic" is accessing the
- * built-in interable method stored in each set. You can do this using `.values().next().value`.
- * It's the same as accessing a value from a generator function.
- */
 export class LRUCache {
   keyMap = new Map<number, number>();
 
   constructor(public capacity: number) {}
 
   get(key: number): number {
+    // If no value exists in the map, return -1
     if (!this.keyMap.has(key)) return -1;
 
-    const value = this.keyMap.get(key)!;
-    this.setMRU(key, value);
-
-    return value;
+    // "Use" the key in the LRU cache, and return the value
+    this.use(key);
+    return this.keyMap.get(key)!;
   }
 
   put(key: number, value: number): void {
-    this.setMRU(key, value);
-
-    if (this.keyMap.size > this.capacity) {
-      this.removeLFU();
-    }
-  }
-
-  setMRU(key: number, value: number) {
+    // Delete previous value from map
     if (this.keyMap.has(key)) {
       this.keyMap.delete(key);
     }
+
+    // Set the new key
+    this.keyMap.set(key, value);
+
+    // Use it
+    this.use(key);
+
+    // Remove LFU
+    this.removeLFU();
+  }
+
+  use(key: number) {
+    const value = this.keyMap.get(key)!;
+    this.keyMap.delete(key);
     this.keyMap.set(key, value);
   }
 
   removeLFU() {
+    if (this.keyMap.size <= this.capacity) return;
+
     const lfuKey = this.keyMap.keys().next().value;
     this.keyMap.delete(lfuKey);
   }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */

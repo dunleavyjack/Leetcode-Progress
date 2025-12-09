a342e73b07c42b88b829752eba171fdc900d94c1
dunleavyjack <dunleavyjack@gmail.com>
2025-12-08 23:08:26 -0500

    ts solution for largest rectangle in histogram

---
+++ Solutions/Neetcode150/Stack/6-LargestRectangleInHistogram/solution.ts
@@ -0,0 +1,21 @@
+function largestRectangleArea(heights: number[]): number {
+  let maxArea = 0;
+  const stack: [number, number][] = []; // [index, height];
+
+  for (let i = 0; i < heights.length; i++) {
+    let start = i;
+    while (stack.length && stack[stack.length - 1][1] > heights[i]) {
+      const [index, height] = stack.pop()!;
+      maxArea = Math.max(maxArea, height * (i - index));
+      start = index;
+    }
+
+    stack.push([start, heights[i]]);
+  }
+
+  for (const [index, height] of stack) {
+    maxArea = Math.max(maxArea, height * (heights.length - index));
+  }
+
+  return maxArea;
+}

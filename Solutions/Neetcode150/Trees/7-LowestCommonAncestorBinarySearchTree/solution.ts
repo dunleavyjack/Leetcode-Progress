40893547c79eb8e2c2cbc4abc93e2740686fa8d1
dunleavyjack <dunleavyjack@gmail.com>
2025-12-08 21:49:10 -0500

    ts solution for lowest common ancestor of binary search tree

---
+++ Solutions/Neetcode150/Trees/7-LowestCommonAncestorBinarySearchTree/solution.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from "../../../../Types/TreeNode/TreeNode";
+
+export function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null,
+): TreeNode | null {
+  if (!root) return null;
+
+  if (p!.val < root.val && q!.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+
+  if (p!.val > root.val && q!.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root;
+}

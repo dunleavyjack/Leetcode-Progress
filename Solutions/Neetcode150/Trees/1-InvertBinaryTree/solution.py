48783551de570194cea0dcbfa7283de5f8d4a561
dunleavyjack <dunleavyjack@gmail.com>
2026-01-08 23:36:54 -0500

    more python solutions for binary search problems and trees

---
+++ Solutions/Neetcode150/Trees/1-InvertBinaryTree/solution.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from Types.TreeNode.TreeNode import TreeNode
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root

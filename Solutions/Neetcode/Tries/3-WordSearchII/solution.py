d133893e932760b072c001faab2aebe8c7147603
dunleavyjack <dunleavyjack@gmail.com>
2026-02-11 20:51:03 -0500

    more py solutions

--- Solutions/Neetcode/Tries/3-WordSearchII/solution.py
+++ Solutions/Neetcode/Tries/3-WordSearchII/solution.py
@@ -1,54 +1,58 @@
 from typing import List
 
-
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.end = False
+        self.is_word = False
 
+    def add_word(self, word):
+        curr = self
+
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        
+        curr.is_word = True
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
+        
+        for word in words:
+            root.add_word(word)
+
         ROWS = len(board)
         COLS = len(board[0])
 
-        for word in words:
-            curr = root
-            for char in word:
-                if char not in curr.children:
-                    curr.children[char] = TrieNode()
-                curr = curr.children[char]
-
-            curr.end = True
-
-        res, visited = set(), set()
+        res = set()
 
         def dfs(r, c, node, word):
             if (
-                r < 0
-                or c < 0
-                or r >= ROWS
-                or c >= COLS
-                or (r, c) in visited
-                or board[r][c] not in node.children
+                r < 0 or
+                c < 0 or
+                r >= ROWS or
+                c >= COLS or
+                board[r][c] not in node.children
             ):
                 return
 
-            visited.add((r, c))
-            node = node.children[board[r][c]]
-            word += board[r][c]
-            if node.end:
+            char = board[r][c]
+            word += char
+            node = node.children[char]
+
+            if node.is_word:
                 res.add(word)
 
+            board[r][c] = "#"
             dfs(r + 1, c, node, word)
             dfs(r - 1, c, node, word)
             dfs(r, c + 1, node, word)
             dfs(r, c - 1, node, word)
-            visited.remove((r, c))
+            board[r][c] = char
 
         for r in range(ROWS):
             for c in range(COLS):
                 dfs(r, c, root, "")
 
-        return list(res)
+        return list(res) 

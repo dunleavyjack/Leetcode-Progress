f8f386ae9562e098d03d96f9353b25102b927dea
dunleavyjack <dunleavyjack@gmail.com>
2026-02-23 08:17:27 -0500

    better py two pointer solution

--- Solutions/Neetcode/TwoPointers/1-ValidPalindrome/solution.py
+++ Solutions/Neetcode/TwoPointers/1-ValidPalindrome/solution.py
@@ -3,21 +3,16 @@ class Solution:
         l, r = 0, len(s) - 1
 
         while l <= r:
-            while l < r and not self.isAlphaNum(s[l]):
+            while l < r and not s[l].isalnum():
                 l += 1
-            while r > l and not self.isAlphaNum(s[r]):
+            while r > l and not s[r].isalnum():
                 r -= 1
 
             if s[l].lower() != s[r].lower():
                 return False
+
             l += 1
             r -= 1
 
         return True
 
-    def isAlphaNum(self, char: str) -> bool:
-        return (
-            ord("a") <= ord(char) <= ord("z")
-            or ord("A") <= ord(char) <= ord("Z")
-            or ord("0") <= ord(char) <= ord("9")
-        )

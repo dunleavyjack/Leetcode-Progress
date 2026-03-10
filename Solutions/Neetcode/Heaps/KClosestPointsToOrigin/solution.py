967fc83a7868b15b4b7c5e6b2638d5ef00c0719e
dunleavyjack <dunleavyjack@gmail.com>
2026-03-10 07:57:09 -0400

    max heap solution for k closest points to origin

---
+++ Solutions/Neetcode/Heaps/KClosestPointsToOrigin/solution.py
@@ -0,0 +1,20 @@
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+
+        for x, y in points:
+            distance = -((x**2) + (y**2))
+            heappush(heap, (distance, x, y))
+
+            if len(heap) > k:
+                heappop(heap)
+
+        res = []
+        for distance, x, y in heap:
+            res.append([x, y])
+
+        return res

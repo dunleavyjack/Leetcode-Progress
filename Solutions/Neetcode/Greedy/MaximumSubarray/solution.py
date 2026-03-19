b5cead7032b26b9dd00a2672afa4f4ccb7118ca0
dunleavyjack <dunleavyjack@gmail.com>
2026-03-19 08:32:34 -0400

    py solution for max subarray using kadane's algo

---
+++ Solutions/Neetcode/Greedy/MaximumSubarray/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float("-inf")
+        curr_sum = 0
+
+        # Kandane's algo.
+        for num in nums:
+            curr_sum += num
+            max_sum = max(max_sum, curr_sum)
+
+            if curr_sum < 0:
+                curr_sum = 0
+
+        return int(max_sum)

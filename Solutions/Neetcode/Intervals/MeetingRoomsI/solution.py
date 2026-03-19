99addcbc503c11da2eefbe4d395e993a6c09c5e3
dunleavyjack <dunleavyjack@gmail.com>
2026-03-19 07:35:16 -0400

    updated py solution for merge intervals

--- Solutions/Neetcode/Intervals/MeetingRoomsI/solution.py
+++ Solutions/Neetcode/Intervals/MeetingRoomsI/solution.py
@@ -5,13 +5,11 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort(key=lambda interval: interval[0])
 
-        if not intervals:
-            return True
+        for i in range(1, len(intervals)):
+            prev_end = intervals[i - 1][1]
+            start = intervals[i][0]
 
-        prev_end = intervals[0][1]
-        for start, end in intervals[1:]:
             if start < prev_end:
                 return False
-            prev_end = end
 
         return True
--- Solutions/Neetcode/Intervals/MergeIntervals/solution.py
+++ Solutions/Neetcode/Intervals/MergeIntervals/solution.py
@@ -5,14 +5,18 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda interval: interval[0])
 
-        res = []
-        for start, end in intervals:
-            if res:
-                last_end = res[-1][1]
-                if start <= last_end:
-                    res[-1][1] = max(last_end, end)
-                    continue
+        if not intervals:
+            return []
 
-            res.append([start, end])
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            prev_end = res[-1][1]
+            start, end = intervals[i]
+
+            if start > prev_end:
+                res.append(intervals[i])
+            else:
+                res[-1][1] = max(end, prev_end)
 
         return res
